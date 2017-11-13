e7dbadb9092c530357e86a94554077470f55fa40
quietcoder <1642965215@qq.com>
2017-11-13 23:57:01 +0800

    add: heap

--- build/list.js
+++ build/list.js
@@ -7,6 +7,12 @@ module.exports = [
         id: 224,
         question: 'https://leetcode.com/problems/basic-calculator/description/',
         answer: 'basic-calculator'
+      },
+      {
+        title: 'Kth Largest Element in an Array',
+        id: 215,
+        question: 'https://leetcode.com/problems/kth-largest-element-in-an-array/description/',
+        answer: 'kth-largest-element-in-an-array'
       }
     ]
   }
